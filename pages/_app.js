11c97c94069e0916224e742f85ca8839ccb952ed
Viswanathan Saravanan <saravanan.resume@gmail.com>
2024-06-27 23:15:14 +0800

    unable to remove _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,16 @@
-// import { Provider } from "react-redux";
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-// import store from "../redux/store.js.old";
-import '../styles/globals.css'
-// import { createRoot, hydrateRoot } from 'react-dom/client'
-// import App from 'next/app';
 
-// import { createRoot } from 'react-dom/client'
-// const container = document.getElementById('app')
-// const root = createRoot(container)
-// root.render(<App tab='home' />)
+import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    // <Provider>
     <div className='wrapper'>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
     </div>
-    // </Provider>
   )
 }
-
-// // This is only for client-side rendering
-// if (typeof window !== 'undefined') {
-//   const root = document.getElementById('__next')
-//   if (root.hasChildNodes()) {
-//     hydrateRoot(root, <MyApp />)
-//   } else {
-//     createRoot(root).render(<MyApp />)
-//   }
-// }
 
 export default MyApp
