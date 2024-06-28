a1a530fc48ffc05cf4028735619cbf769a14d6ae
Viswanathan Saravanan <saravanan.resume@gmail.com>
2024-06-28 14:27:01 +0800

    height and width

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -5,7 +5,7 @@ import styles from '../styles/CategoryCard.module.css';
 const CategoryCard = ({ image, name }) => {
   return (
     <div className={styles.card}>
-      <Image className={styles.image} src={image} height={700} width={1300} />
+      <Image className={styles.image} src={image} height={400} width={550} />
       <Link href={`/category/${name.toLowerCase()}`}>
         <div className={styles.info}>
           <h3>{name}</h3>
