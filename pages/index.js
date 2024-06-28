b11c393d2214a50e66e7f510a7a946ddbbde04e5
Viswanathan Saravanan <saravanan.resume@gmail.com>
2024-06-28 16:33:59 +0800

    move the images to public

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-import CategoryCard from '../components/CategoryCard';
-import styles from '../styles/Home.module.css';
+import CategoryCard from '../components/CategoryCard'
+import styles from '../styles/Home.module.css'
 
 const HomePage = () => {
   return (
     <main className={styles.container}>
       <div className={styles.small}>
-        <CategoryCard image="https://imgur.com/uKQqsuA.png" name="Xbox" />
-        <CategoryCard image="https://imgur.com/3Y1DLYC.png" name="PS5" />
-        <CategoryCard image="https://imgur.com/Dm212HS.png" name="Switch" />
+        <CategoryCard image='/images/uKQqsuA.png' name='Xbox' />
+        <CategoryCard image='/images/3Y1DLYC.png' name='PS5' />
+        <CategoryCard image='/images/Dm212HS.png' name='Switch' />
       </div>
       <div className={styles.large}>
-        <CategoryCard image="https://imgur.com/qb6IW1f.png" name="PC" />
-        <CategoryCard
-          image="https://imgur.com/HsUfuRU.png"
-          name="Accessories"
-        />
+        <CategoryCard image='/images/qb6IW1f.png' name='PC' />
+        <CategoryCard image='/images/HsUfuRU.png' name='Accessories' />
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
